29093c6b56f12e8aa8b308dae2681a701db8fa84
0CrazyLove <axelvasquez582@gmail.com>
2025-10-11 18:47:14 -0500

    ....

--- EventSync/Services/GoogleCalendarService.cs
+++ EventSync/Services/GoogleCalendarService.cs
@@ -1,6 +1,7 @@
+using System.Reflection.Metadata.Ecma335;
 using Google.Apis.Auth.OAuth2;
 using Google.Apis.Calendar.v3;
-
+using Google.Apis.Calendar.v3.Data;
 using Google.Apis.Services;
 
 /// <summary>
@@ -8,27 +9,31 @@ using Google.Apis.Services;
 /// </summary>
 public class GoogleCalendarService : ICalendarService
 {
-    private const string ApplicationName = "GoogleCalendarDemo"; //por que se pone const para la variable ApplicationName
 
     /// <summary>
     /// Obtiene una lista de eventos del calendario principal del usuario.
     /// </summary>
     /// <param name="accessToken">El token de acceso de OAuth 2.0 para autorizar la solicitud a la API.</param>
     /// <returns>Una lista de eventos del calendario.</returns>
-    public async Task<IList<GoogleCalendar>> GetEventsAsync(string accessToken)
+    /// 
+    private CalendarService InitializerService(string accessToken)
     {
         var credential = GoogleCredential.FromAccessToken(accessToken);
-    
+
         var initializer = new BaseClientService.Initializer()
         {
-            HttpClientInitializer = credential,
-            ApplicationName = ApplicationName
+            HttpClientInitializer = credential
         };
-    
         var CalendarService = new CalendarService(initializer);
-    
-        var events = await CalendarService.Events.List("primary").ExecuteAsync(); 
-    
+
+        return CalendarService;
+    }
+    public async Task<IList<GoogleCalendar>> GetEventsAsync(string accessToken)
+    {
+        var CalendarService =  InitializerService(accessToken);
+
+        var events = await CalendarService.Events.List("primary").ExecuteAsync();
+
         var resultado = events.Items.Select(e => new GoogleCalendar
         {
             Id = e.Id,
@@ -38,9 +43,17 @@ public class GoogleCalendarService : ICalendarService
             StartDate = e.Start.DateTimeDateTimeOffset?.DateTime ?? (DateTime.TryParse(e.Start.Date, out var startDate) ? startDate : null),
             EndDate = e.End.DateTimeDateTimeOffset?.DateTime ?? (DateTime.TryParse(e.End.Date, out var endDate) ? endDate : null),
             Organizer = e.Organizer?.DisplayName ?? e.Organizer?.Email ?? string.Empty,
-            
+
         }).ToList();
-    
+
         return resultado;
     }
+
+    public async Task<Event> CreateEventAsync(Event calendarEvent, string accessToken)
+    {
+        var calendarService =  InitializerService(accessToken);
+                                                                                                    
+
+    }
+
 }
